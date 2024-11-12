d6d9c4233cdc4598f1d24a688578ddfba2d4339e
LisbethMendoza <lisbethango223019@gmail.com>
2024-11-12 17:18:11 -0400

    Feat: screen and subtitules

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -5,23 +5,47 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 
 class AuthGate extends StatelessWidget {
-  const AuthGate({super.key});
+ const AuthGate({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return SignInScreen(
-            providers: [
-              EmailAuthProvider(),
-            ],
-          );
-        }
+ @override
+ Widget build(BuildContext context) {
+   return StreamBuilder<User?>(
+     stream: FirebaseAuth.instance.authStateChanges(),
+     builder: (context, snapshot) {
+       if (!snapshot.hasData) {
+         return SignInScreen(
+           providers: [
+              EmailAuthProvider()
+           ],
+           headerBuilder: (context, constraints, shrinkOffset) {
+             return Padding(
+               padding: const EdgeInsets.all(20),
+               child: AspectRatio(
+                 aspectRatio: 1,
+                 child: Image.asset('flutterfire_300x.png'),
+               ),
+             );
+           },
+           subtitleBuilder: (context, action) {
+             return Padding(
+               padding: const EdgeInsets.symmetric(vertical: 8.0),
+               child: action == AuthAction.signIn
+                   ? const Text('Welcome to FlutterFire, please sign in!')
+                   : const Text('Welcome to Flutterfire, please sign up!'),
+             );
+           },
+         );
+       }
 
-        return const HomeScreen();
-      },
-    );
-  }
+       return const HomeScreen();
+     },
+   );
+ }
 }
+
+
+typedef HeaderBuilder = Widget Function(
+ BuildContext context,
+ BoxConstraints constraints,
+ double shrinkOffset,
+);
